223e83c1ec4b0a3b87a3e7221f8f2956df4e40db
pyk <panyk@hdsxtech.com>
2016-08-24 14:28:34 +0800

    基本功能实现。

--- Bird/WebContent/main.js
+++ Bird/WebContent/main.js
@@ -1,27 +1,32 @@
-var game = new Phaser.Game(400, 490, Phaser.AUTO, 'game_div');
+var game = new Phaser.Game(600, 490, Phaser.AUTO, 'game_div');
 
 var main_state = {
 
     preload: function() { 
         this.game.stage.backgroundColor = '#71c5cf';
-        this.game.load.image('bird', 'assets/bird.png');  
-        this.game.load.image('pipe', 'assets/pipe.png');      
-        this.game.load.image('bg', 'assets/bg_day.png');      
-
+        this.game.load.image('bg', 'assets/bg_day.png');  
+        this.game.load.image('bird', 'assets/bird0_0.png');  
+        this.game.load.image('pipeup', 'assets/pipe_up.png');      
+        this.game.load.image('pipedown', 'assets/pipe_down.png');      
         // Load jump sound
         this.game.load.audio('jump', 'assets/jump.wav');
     },
 
-    create: function() { 
+    create: function() {
+    	var bg = game.add.tileSprite(0,0,game.width,game.height,'bg'); //当作背景的tileSprite 
+    	//bg.autoScroll(-10,0); //让背景动起来
+    	
         var space_key = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         space_key.onDown.add(this.jump, this); 
 
-        this.pipes = game.add.group();
-        this.pipes.createMultiple(20, 'pipe');  
+        this.pipeups = game.add.group();
+        this.pipeups.createMultiple(20, 'pipeup');  
+        this.pipedowns = game.add.group();
+        this.pipedowns.createMultiple(20, 'pipedown');  
         this.timer = this.game.time.events.loop(1500, this.add_row_of_pipes, this);           
-        this.bg = this.game.add.sprite(0, 0, 'bg');
+
         this.bird = this.game.add.sprite(100, 245, 'bird');
-        this.bird.body.gravity.y = 1000; 
+        this.bird.body.gravity.y = 1000;
          // Change the anchor point of the bird
         this.bird.anchor.setTo(-0.2, 0.5);
                
@@ -41,7 +46,8 @@ var main_state = {
         if (this.bird.angle < 20)
             this.bird.angle += 1;
 
-        this.game.physics.overlap(this.bird, this.pipes, this.hit_pipe, null, this);      
+        this.game.physics.overlap(this.bird, this.pipeups, this.hit_pipe, null, this);      
+        this.game.physics.overlap(this.bird, this.pipedowns, this.hit_pipe, null, this);      
     },
 
     jump: function() {
@@ -71,34 +77,31 @@ var main_state = {
         this.game.time.events.remove(this.timer);
 
         // Go trough all the pipes, and stop their movement
-        this.pipes.forEachAlive(function(p){
+        this.pipeups.forEachAlive(function(p){
             p.body.velocity.x = 0;
         }, this);
+        this.pipedowns.forEachAlive(function(p){
+        	p.body.velocity.x = 0;
+        }, this);
     },
-
     restart_game: function() {
         this.game.time.events.remove(this.timer);
         this.game.state.start('main');
     },
-
-    add_one_pipe: function(x, y) {
-        var pipe = this.pipes.getFirstDead();
-        pipe.reset(x, y);
+    add_row_of_pipes: function() {
+    	var x = 600;
+        var y = Math.floor( Math.random()*320 ) - 320;
+        var pipe = this.pipeups.getFirstDead();
+        pipe.reset(x, y+320+200);
         pipe.body.velocity.x = -200; 
         pipe.outOfBoundsKill = true;
-    },
-
-    add_row_of_pipes: function() {
-        var hole = Math.floor(Math.random()*5)+1;
-        
-        for (var i = 0; i < 8; i++)
-            if (i != hole && i != hole +1) 
-                this.add_one_pipe(400, i*60+10);   
-    
+        pipe = this.pipedowns.getFirstDead();
+        pipe.reset(x, y);
+        pipe.body.velocity.x = -200;
+        pipe.outOfBoundsKill = true;   
         this.score += 1;
         this.label_score.content = this.score;  
     },
 };
-
 game.state.add('main', main_state);  
 game.state.start('main'); 
